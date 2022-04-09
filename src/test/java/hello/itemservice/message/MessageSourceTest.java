920d7f7963b7621352fc2b18d216704e669b3e39
LeeJeongGi <apple4rhk@naver.com>
2022-04-09 21:33:35 +0900

    feat : message.properties Test Case Add

--- src/test/java/hello/itemservice/message/MessageSourceTest.java
+++ src/test/java/hello/itemservice/message/MessageSourceTest.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.MessageSource;
+import org.springframework.context.NoSuchMessageException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
 public class MessageSourceTest {
@@ -22,6 +24,12 @@ public class MessageSourceTest {
             
         //then
         assertThat(result).isEqualTo("안녕");
+    }
+
+    @Test
+    public void notFoundMessageCode() {
+        assertThatThrownBy(() -> ms.getMessage("no_code", null, null))
+                .isInstanceOf(NoSuchMessageException.class);
     }
     
 }
